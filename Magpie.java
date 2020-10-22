b084621cf4e1e9b2534ae7242bdcc2dfd3478fcd
Triston Caton <tcaton@localhost.localdomain>
2020-10-21 20:28:37 -0500

    adds more things

--- Magpie.java
+++ Magpie.java
@@ -8,14 +8,30 @@ public class Magpie
    public String getResponse(String statement)
    {
      String response = "";
-     if (statement.indexOf("no") >= 0) {
+     if (statement.indexOf("no") >= 0
+            && (statement.charAt(statement.indexOf("no")) >= 0)){
        response = "Why so negative?";
      } else if (statement.indexOf("mother") >= 0
                  || statement.indexOf("father") >= 0
                  || statement.indexOf("sister") >= 0
                  || statement.indexOf("brother") >= 0) {
        response = "Tell me more about your family.";
-     } else {
+     }
+     else if (statement.indexOf("pizza") >= 0 || statement.indexOf("nachos") >= 0){//Author: Triston Purpose: covers common food choices.
+       response = "Tell me more about food.";
+     }
+     else if (statement.indexOf("nintendo") >= 0 ||statement.indexOf("xbox") >= 0){
+      response = "What video games do you like to play on your console?";//Hunter - makes chatbot interested in video games
+    }
+    else if (statement.indexOf("Java")  >= 0 || statement.indexOf("C++") >= 0){////Author: Triston Purpose: covers common programming languages
+      response = "I also enjoy programming!";
+    }
+    else if (statement.indexOf("dog") >= 0 ||statement.indexOf("cat") >= 0){//Hunter
+      response = "Tell me more about your pets.";
+    }
+    else if (statement.indexOf("Mr.") >= 0 ||statement.indexOf("Mrs.") >= 0 || statement.indexOf("Ms.") >= 0){//Author: Triston Purpose: Fills requirement
+      response = (statement.indexOf("Mr.") >=0 ? "He":"She") + " sounds like a good teacher.";
+    } else {
        response = getRandomResponse();
      }
      return response;
@@ -38,8 +54,7 @@ public class Magpie
        response = "You don't say.";
      }
      return response;
-         }
-
+  }
    public static void main(String[] args)
    {
      Magpie maggie = new Magpie();
