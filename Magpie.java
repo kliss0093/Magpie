c94ce70be4e0fd7ac6e87c18372cb7c746ade1fe
hcarlson0145 <71331334+hcarlson0145@users.noreply.github.com>
2020-10-26 09:18:53 -0500

    Add files via upload

--- Magpie.java
+++ Magpie.java
@@ -1,76 +1,170 @@
-public class Magpie
+/**
+ * A program to carry on conversations with a human user.
+ * This version: 
+ * <ul><li>
+ *    Uses advanced search for keywords 
+ * </li></ul> 
+ *    
+ * @author Laurie White
+ * @version April 2012
+ */
+public class Magpie3
 {
-   public String getGreeting()
-   {
-     return "Hello, let's talk.";
-   }
+	/**
+	 * Get a default greeting
+	 * 
+	 * @return a greeting
+	 */
+	public String getGreeting()
+	{
+		return "Hello, let's talk.";
+	}
 
-   public String getResponse(String statement)
-   {
-     statement.trim();
-     String response = "";
-    if(statement.indexOf("") == 0){
-       return "Say something, please.";
-    }
-     if (statement.indexOf("no") >= 0
-            && (statement.charAt(statement.indexOf("no")) >= 0)){
-       response = "Why so negative?";
-     } else if (statement.indexOf("mother") >= 0
-                 || statement.indexOf("father") >= 0
-                 || statement.indexOf("sister") >= 0
-                 || statement.indexOf("brother") >= 0) {
-       response = "Tell me more about your family.";
-     }
-     else if (statement.indexOf("pizza") >= 0 || statement.indexOf("nachos") >= 0){//Author: Triston Purpose: covers common food choices.
-       response = "Tell me more about food.";
-     }
-     else if (statement.indexOf("nintendo") >= 0 ||statement.indexOf("xbox") >= 0){
-      response = "What video games do you like to play on your console?";//Hunter - makes chatbot interested in video games
-    }
-    else if (statement.indexOf("Java")  >= 0 || statement.indexOf("C++") >= 0){////Author: Triston Purpose: covers common programming languages
-      response = "I also enjoy programming!";
-    }
-    else if (statement.indexOf("dog") >= 0 ||statement.indexOf("cat") >= 0){//Hunter
-      response = "Tell me more about your pets.";
-    }
-    else if (statement.indexOf("Mr.") >= 0 ||statement.indexOf("Mrs.") >= 0 || statement.indexOf("Ms.") >= 0){//Author: Triston Purpose: Fills requirement
-      response = (statement.indexOf("Mr.") >=0 ? "He":"She") + " sounds like a good teacher.";
-    } else {
-       response = getRandomResponse();
-     }
-     return response;
-   }
+	/**
+	 * Gives a response to a user statement
+	 * 
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
+	{
+		String response = "";
+		if (statement.length() == 0)
+		{
+			response = "Say something, please.";
+		}
+		else if (findKeyword(statement, "no") >= 0)
+		{
+			response = "Why so negative?";
+		}
+		else if (findKeyword(statement, "mother") >= 0
+				|| findKeyword(statement, "father") >= 0
+				|| findKeyword(statement, "sister") >= 0
+				|| findKeyword(statement, "brother") >= 0)
+		{
+			response = "Tell me more about your family.";
+		}
+		else
+		{
+			response = getRandomResponse();
+		}
+		return response;
+	}
 
-   private String getRandomResponse()
-   {
-     final int NUMBER_OF_RESPONSES = 4;
-     double r = Math.random();
-     int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
-     String response = "";
+	/**
+	 * Search for one word in phrase. The search is not case
+	 * sensitive. This method will check that the given goal
+	 * is not a substring of a longer string (so, for
+	 * example, "I know" does not contain "no").
+	 *
+	 * @param statement
+	 *            the string to search
+	 * @param goal
+	 *            the string to search for
+	 * @param startPos
+	 *            the character of the string to begin the
+	 *            search at
+	 * @return the index of the first occurrence of goal in
+	 *         statement or -1 if it's not found
+	 */
+	private int findKeyword(String statement, String goal,
+			int startPos)
+	{
+		String phrase = statement.trim().toLowerCase();
+		goal = goal.toLowerCase();
 
-     if (whichResponse == 0) {
-       response = "Interesting, tell me more.";
-     } else if (whichResponse == 1) {
-       response = "Hmmm.";
-     } else if (whichResponse == 2) {
-       response = "Do you really think so?";
-     } else if (whichResponse == 3) {
-       response = "You don't say.";
-     }
-     return response;
-  }
-   public static void main(String[] args)
-   {
-     Magpie maggie = new Magpie();
+		// The only change to incorporate the startPos is in
+		// the line below
+		int psn = phrase.indexOf(goal, startPos);
 
-     System.out.println(maggie.getGreeting());
-     System.out.println(">My mother and I talked last night.");
-     System.out.println(maggie.getResponse("My mother and I talked last night."));
-     System.out.println(">I said no.");
-     System.out.println(maggie.getResponse("I said no!"));
-     System.out.println(">The weather is nice.");
-     System.out.println(maggie.getResponse("The weather is nice."));
-     System.out.println(">Do you know my brother?");
-     System.out.println(maggie.getResponse("Do you know my brother?"));
-   }
+		// Refinement--make sure the goal isn't part of a
+		// word
+		while (psn >= 0)
+		{
+			// Find the string of length 1 before and after
+			// the word
+			String before = " ", after = " ";
+			if (psn > 0)
+			{
+				before = phrase.substring(psn - 1, psn);
+			}
+			if (psn + goal.length() < phrase.length())
+			{
+				after = phrase.substring(
+						psn + goal.length(),
+						psn + goal.length() + 1);
+			}
+
+			// If before and after aren't letters, we've
+			// found the word
+			if (((before.compareTo("a") < 0) || (before
+					.compareTo("z") > 0)) // before is not a
+											// letter
+					&& ((after.compareTo("a") < 0) || (after
+							.compareTo("z") > 0)))
+			{
+				return psn;
+			}
+
+			// The last position didn't work, so let's find
+			// the next, if there is one.
+			psn = phrase.indexOf(goal, psn + 1);
+
+		}
+
+		return -1;
+	}
+
+	/**
+	 * Search for one word in phrase. The search is not case
+	 * sensitive. This method will check that the given goal
+	 * is not a substring of a longer string (so, for
+	 * example, "I know" does not contain "no"). The search
+	 * begins at the beginning of the string.
+	 * 
+	 * @param statement
+	 *            the string to search
+	 * @param goal
+	 *            the string to search for
+	 * @return the index of the first occurrence of goal in
+	 *         statement or -1 if it's not found
+	 */
+	private int findKeyword(String statement, String goal)
+	{
+		return findKeyword(statement, goal, 0);
+	}
+
+	/**
+	 * Pick a default response to use if nothing else fits.
+	 * 
+	 * @return a non-committal string
+	 */
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
+		String response = "";
+
+		if (whichResponse == 0)
+		{
+			response = "Interesting, tell me more.";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmm.";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so?";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "You don't say.";
+		}
+
+		return response;
+	}
+
 }
